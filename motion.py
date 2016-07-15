469f6af15e56382d7f0a572a88d5e54476861b9d
Rishabh Manocha <rmanoch@elance-odesk.com>
2016-07-15 11:39:29 -0700

    initial commit

---
+++ motion.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+MOTION_GPIO=7
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MOTION_GPIO, GPIO.IN)
+
+try:
+    while True:
+        if GPIO.input(MOTION_GPIO):
+            print "motion detected"
+                else:
+                    print "nothing"
+                        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print " quit"
+        GPIO.cleanup()
